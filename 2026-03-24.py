debd208f421b4a9631217c356fe98a14670148be
Alumnos Santiago Centro <alumnos09@inacap.cl>
2026-03-24 20:16:58 -0300

    hola

--- 2026-03-24.py
+++ 2026-03-24.py
@@ -9,6 +9,9 @@ ano_actual = int (input ( "ingresa el año en que nos encontramos "))
 ano_nacimiento= int (input ("ingresa tu año de nacimiento"))
 edad_actual = ano_actual - ano_nacimiento
 #SALIDA 
+print ("Su edad es", edad_actual,"años")
+print(f"Su edad es: {edad_actual} años")  #La f en Python se usa para crear lo que se llama un f-string (cadena formateada).
+# Sirve para insertar variables dentro de un texto de forma fácil.
 
-print(f"Su edad es: {edad_actual} años")
+
  
