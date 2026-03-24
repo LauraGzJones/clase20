abbacd23d9db33c02d519620732627898abaf6cb
Alumnos Santiago Centro <alumnos09@inacap.cl>
2026-03-24 19:39:54 -0300

    hola

---
+++ 20.py
@@ -0,0 +1,10 @@
+#Declaración de variables
+ed = 0
+ed_prox_anio = 0
+
+ed = int(input("Su edad: ")) #int convierte un cadena(string, texto, alfanumérico) a número entero, float convierte a decimal
+
+ed_prox_anio = ed + 1
+
+#Salida
+print("Su edad actual es", ed, "el próximo año vas a tener", ed_prox_anio)
